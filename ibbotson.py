2db85fd77a3818b9fcab0ac7599bdcb2883aa485
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2026-09-17 18:32:28 -0700

    Add files via upload

---
+++ ibbotson.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from statsmodels.api import OLS
+import matplotlib.pyplot as plt
+import scipy
+from statsmodels.api import stats
+
+def verification(data):
+    print('Shapiro-Wilk p = ', scipy.stats.shapiro(data)[1])
+    print('Jarque-Bera p = ', scipy.stats.jarque_bera(data)[1])
+    print('ACF p-value for Ljung-Box test = ', stats.acorr_ljungbox(data, lags = [5, 10])['lb_pvalue'].values)
+    print('Same for absolute values = ', stats.acorr_ljungbox(abs(data), lags = [5, 10])['lb_pvalue'].values)
+
+# reading the data file
+DF = pd.read_excel('ibbotson.xlsx')
+rate = DF['Long'].values
+trueRet = np.log(1 + DF['Returns'].values[1:] - 0.01 * rate[:-1])
+
+Reg = OLS(trueRet[:-10], pd.DataFrame({'const' : 1, 'duration' : np.diff(np.log(1 + rate * 0.01))}).iloc[:-10]).fit()
+print(Reg.summary())
+verification(Reg.resid)
