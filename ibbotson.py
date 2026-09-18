8309746c993b8ee16caba4159ebf8a4af855b41a
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2026-09-17 18:55:56 -0700

    Add files via upload

--- ibbotson.py
+++ ibbotson.py
@@ -16,6 +16,17 @@ DF = pd.read_excel('ibbotson.xlsx')
 rate = DF['Long'].values
 trueRet = np.log(1 + DF['Returns'].values[1:] - 0.01 * rate[:-1])
 
+# Regression only for Ibbotson data 1926-2015
 Reg = OLS(trueRet[:-10], pd.DataFrame({'const' : 1, 'duration' : np.diff(np.log(1 + rate * 0.01))}).iloc[:-10]).fit()
+print('Ibbotson data 1926-2015')
 print(Reg.summary())
 verification(Reg.resid)
+print('stderr = ', np.std(Reg.resid))
+
+# Regression for augmented data if add 8-year zero-coupon Treasury rates 2016-2025
+Reg = OLS(trueRet, pd.DataFrame({'const' : 1, 'duration' : np.diff(np.log(1 + rate * 0.01))})).fit()
+print('Augmented data: Ibbotson 1926-2015')
+print('And zero-coupon Treasury bonds 8 years 2016-2025')
+print(Reg.summary())
+verification(Reg.resid)
+print('stderr = ', np.std(Reg.resid))
